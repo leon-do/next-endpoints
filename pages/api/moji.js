b14fcbc898032e17013e39d42b12983d857c5103
Leon Do <leondo123@gmail.com>
2023-10-03 11:03:44 -0400

    Update moji.js

--- pages/api/moji.js
+++ pages/api/moji.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   const audio = `https://gitlab.com/dexoshi/science-audio/-/raw/main/${
     Math.floor(Math.random() * 55) + 1
   }.mp3`;
-  const video1 = `https://gitlab.com/leon-do/find.emoji/-/raw/main/emojis/${random}.jpeg`;
+  const video1 = `https://bafybeihsablkej567ntnsmbo2paj26lgdouleg5symgtc3dafzpgihdqy4.ipfs.nftstorage.link/${random}.jpeg`;
   const output = { audio, video1 };
   res.json(output);
 }
